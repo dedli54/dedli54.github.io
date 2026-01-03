ab3d1814fc5053c9cbae7ca567040f308024226b
dedli54 <betogarciacamacho@gmail.com>
2026-01-03 01:16:02 -0600

    fix: clean up

--- PanaderiaIBM/js/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Update copyright year dynamically
-    const copyrightYear = document.querySelector('.bakery-footer p');
-    if (copyrightYear) {
-        const currentYear = new Date().getFullYear();
-        copyrightYear.innerHTML = copyrightYear.innerHTML.replace('2025', currentYear);
-    }
-});
